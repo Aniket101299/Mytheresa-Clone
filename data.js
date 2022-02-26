0781116b34d516068265b9bc405afae21a3ecc6c
Aniket101299 <91543059+Aniket101299@users.noreply.github.com>
2022-02-26 10:28:50 +0530

    done with website

---
+++ data.js
@@ -0,0 +1,172 @@
+// {
+//   "brand": "FUSALP",
+//   "title": "Abelban ski jacket",
+//   "price": 429,
+//   "size": "s",
+//   "color": "black",
+//   "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/a7/P00610567.jpg",
+//   "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/a7/P00610567_b1.jpg"
+// }
+//   {
+//     "brand":"FUSALP",
+//     "title":"Wengen IV wool sweater",
+//     "price":188,
+//     "size": "s",
+//     "color": "white",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/44/P00610572_b1.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/44/P00610572.jpg"
+//   }
+//   {
+//     "brand":"FUSALP",
+//     "title":"Tomaso ski pants",
+//     "price":177,
+//     "size": "l",
+//     "color": "black",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/de/P00610569.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/de/P00610569_b1.jpg"
+//   }
+//   {
+//     "brand":"AZTECH MOUNTAIN",
+//     "title":"Dale of Aspen vest",
+//     "price":398,
+//     "size": "m",
+//     "color": "black",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/76/P00501270.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/76/P00501270_b1.jpg"
+//   }
+//   {
+//     "brand":"FUSALP",
+//     "title":"Whistler down-filled jacket",
+//     "price":647,
+//     "size": "l",
+//     "color": "grey",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/75/P00514699.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/75/P00514699_b1.jpg"
+//   }
+//   {
+//     "brand":"AND WANDER",
+//     "title":"Diamond-quilted down jacket",
+//     "price":495,
+//     "size": "xxl",
+//     "color": "brown",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/62/P00583238.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/62/P00583238_b1.jpg"
+//   }
+//   {
+//     "brand":"AND WANDER",
+//     "title":"And Wander x Salomon OUTpath CSWP sneakers",
+//     "price":147,
+//     "size": "eu38",
+//     "color": "brown",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/09/P00583252.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/09/P00583252_d2.jpg"
+//   }
+//   {
+//     "brand":"STONE ISLAND",
+//     "title":"Nylon Metal Down-TC jacket",
+//     "price":552,
+//     "size": "l",
+//     "color": "green",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/ae/P00593954.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/ae/P00593954_b1.jpg"
+//   }
+//   {
+//     "brand":"STONE ISLAND",
+//     "title":"Garment-dyed Down-TC nylon reps jacket",
+//     "price":482,
+//     "size": "xl",
+//     "color": "blue",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/5a/P00593940.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/5a/P00593940_b1.jpg"
+//   }
+//   {
+//     "brand":"THE NORTH FACE",
+//     "title":"1996 Retro Nuptse jacket",
+//     "price":153,
+//     "size": "m",
+//     "color": "yellow",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/44/P00593152_b1.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/44/P00593152.jpg"
+//   }
+//   {
+//     "brand":"STONE ISLAND",
+//     "title":"Technical jacket",
+//     "price":517,
+//     "size": "3xl",
+//     "color": "red",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/7b/P00593953.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/7b/P00593953_b1.jpg"
+//   }
+//   {
+//     "brand":"STONE ISLAND",
+//     "title":"Modified Panama 6/3 HT Nylon Down-TC jacket",
+//     "price":493,
+//     "size": "m",
+//     "color": "green",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/ea/P00593949.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/ea/P00593949_b1.jpg"
+//   }
+//   {
+//     "brand":"THE NORTH FACE",
+//     "title":"M66 down-padded jacket",
+//     "price":117,
+//     "size": "s",
+//     "color": "green",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/1a/P00593126.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/1a/P00593126_b1.jpg"
+//   }
+//   {
+//     "brand":"FUSALP",
+//     "title":"Perfortex™ Ranger pants",
+//     "price":293,
+//     "size": "s",
+//     "color": "green",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/0c/P00514957.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/0c/P00514957_b1.jpg"
+//   }
+//   {
+//     "brand":"THE NORTH FACE",
+//     "title":"Sherpa Nuptse fleece jacket",
+//     "price":188,
+//     "size": "l",
+//     "color": "blue",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/49/P00593121.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/49/P00593121_b1.jpg"
+//   }
+//   {
+//     "brand":"AND WANDER",
+//     "title":"Stretch Shell jacket",
+//     "price":249,
+//     "size": "xl",
+//     "color": "grey",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/a2/P00583235.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/a2/P00583235_b1.jpg"
+//   }
+//   {
+//     "brand":"STONE ISLAND",
+//     "title":"Rain Camo Reflective Naslan Light Watro parka",
+//     "price":1473,
+//     "size": "m",
+//     "color": "beige",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/8d/P00593951.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/8d/P00593951_b1.jpg"
+//   }
+//   {
+//     "brand":"STONE ISLAND",
+//     "title":"Rain Camo Reflective Naslan Light overshirt",
+//     "price":346,
+//     "size": "l",
+//     "color": "beige",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/0a/P00593903.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/0a/P00593903_b1.jpg"
+//   }
+//   {
+//     "brand":"AZTECH MOUNTAIN",
+//     "title":"Hayden 3L Shell jacket",
+//     "price":329,
+//     "size": "m",
+//     "color": "white",
+//     "img": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/41/P00616622.jpg",
+//     "Hoverimg": "https://img.mytheresa.com/560/560/33/jpeg/catalog/product/41/P00616622_b1.jpg"
+//   }
+  
